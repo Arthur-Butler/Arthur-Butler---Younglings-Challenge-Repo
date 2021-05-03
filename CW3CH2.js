bae6a018ae282ebb1c0b96fd1da24bb9638c73a6
CPTWEX012 <62343020+CPTWEX012@users.noreply.github.com>
2021-05-03 10:39:30 +0200

    Add files via upload

---
+++ CW3CH2.js
@@ -0,0 +1,73 @@
+var Sum = 0;
+var Ave = 0;
+var Med = 0;
+var Mod = 0;
+var Ran = 0;
+const NumNo = prompt('Enter the number of values:');
+const Type = prompt('Enter the type of operation (Sum, Average, Median, Mode, Range):');
+const ValArr = [];
+for (let i = 0; i < NumNo; i++) {
+    Value = prompt(`Enter value (${i + 1}/${NumNo})`)
+    PushArr = ValArr.push(Value)
+};
+
+if (Type == 'Sum') {
+    for (let i = 0; i < NumNo; i++) {
+        Sum = parseInt(Sum) + parseInt(ValArr[i]);
+    }
+    console.log(`Sum is: ${Sum}`);
+};
+
+if (Type == 'Average') {
+    for (let i = 0; i < NumNo; i++) {
+        Sum = parseInt(Sum) + parseInt(ValArr[i]);
+    }
+    Ave = Sum / NumNo
+    console.log(`Average is: ${Ave}`);
+};
+
+if (Type == 'Average') {
+    for (let i = 0; i < NumNo; i++) {
+        Sum = parseInt(Sum) + parseInt(ValArr[i]);
+    }
+    Ave = Sum / NumNo
+    console.log(`Average is: ${Ave}`);
+};
+/*
+switch (OpType = Type) {
+    case (Type = 'Sum'):
+        Sum = valArr.reduce(function (a, b) {
+            return a + b;
+        }, 0);
+        console.log(Sum);
+        break;
+
+    case (Type = 'Average'):
+        var Product = valArr.reduce(function (a, b) {
+            return a * b;
+        }, 0);
+        var Average = Product / NumNo
+        console.log(Average);
+        break;
+};
+
+case (Type = 'Median'):
+    console.log(sum);
+    break;
+
+case (Type = 'Mode'):
+    for (let i = 0; i < NumNo; i++) {
+        const sum = +valArr[i];
+    };
+    console.log(sum);
+    break;
+
+case (Type = 'Range'):
+    for (let i = 0; i < NumNo; i++) {
+        const sum = +valArr[i];
+    };
+    console.log(sum);
+    break;
+}
+*/
+
